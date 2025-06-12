b7b3fd62aad9ac9e47ceb919efc2c1c2aa2d29e6
Carmen <cvieher@gmail.com>
2025-06-12 20:52:13 +0200

    añadiendo .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
@@ -8,9 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">¡Hola Tailwind + React + Vite!</h1>
-    </div>
+      
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
-import { StrictMode } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './styles/main.css'
-import App from './App.jsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/main.css';
+import App from './App.jsx';
 
 
-createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(container)
+  
+  root.render(
   <StrictMode>
     <BrowserRouter>
-      
-        <App />
+       
+          <App />
+       
+        
       
     </BrowserRouter>
   </StrictMode>,
