bc69459d4aeeaeb632a7e24b6aeaedf255b9af25
Jose Luis Arias <dumas6es@gmail.com>
2025-06-18 01:38:33 +0200

    arreglando login page

---
+++ src/services/users/LoginUserService.js
@@ -0,0 +1,21 @@
+const API_BASE = "http://localhost:3000";
+
+const ApiService = {
+  login: async (email, password) => {
+    const res = await fetch(${API_BASE}/users/login, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.message || "Error en el login");
+    }
+
+    const data = await res.json();
+    return data; // aquí debe tener el token
+  },
+};
+
+export default ApiService;
